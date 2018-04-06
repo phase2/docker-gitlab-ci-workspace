b76e3ae7fe33cb0e65e790093775ceaba273a1bc
Adam Ross <aross@phase2technology.com>
2018-04-05 19:48:17 -0700

    Initial commit.

---
+++ root/versions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+echo "---------------"
+echo "Installed Tools"
+echo "---------------"
+docker --version
+docker-compose --version
+python --version
+pip --version
+echo "kubectl:" $(kubectl version --client --short)
+echo "helm:" $(helm version --client --short)
