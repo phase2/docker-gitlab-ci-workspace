45a12333c2e661b679942a04e777462e6230ad6a
Chris Johnson <cjohnson@phase2technology.com>
2018-11-10 13:10:13 -0500

    Align helm version to tiller version in our cluster. Set up to allow docker version 18 and latest

--- build.sh
+++ build.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-docker build -t outrigger/gitlab-ci-workspace .
+
+set -e
+
+# Get the versions to build
+
+# If they are passed as args
+if [ $# -gt 0 ]; then
+  buildDirs="$@"
+else
+  # else, get all the relevant dirs
+  buildDirs=$(find . -type d -name 'docker-*' | sed 's/^\.\///')
+fi
+echo $buildDirs
+
+# Build each one of them
+for version in $buildDirs; do
+  pushd $version
+  docker build -t outrigger/gitlab-ci-workspace:${version} .
+  popd
+done
